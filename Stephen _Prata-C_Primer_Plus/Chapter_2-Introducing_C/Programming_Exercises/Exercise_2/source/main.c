255a9d11b55f141b610ed34354018dffc9a3c8ac
Viacheslav Y <155384480+yef-codes@users.noreply.github.com>
2024-01-06 10:17:25 +0200

    Added S.Prata-Ch.2-Ex.2

---
+++ Stephen _Prata-C_Primer_Plus/Chapter_2-Introducing_C/Programming_Exercises/Exercise_2/source/main.c
@@ -0,0 +1,21 @@
+/*
+ ============================================================================
+ Name        : S.Prata-C_Primer_Plus-ch.2-ex.2
+ Author      : Viacheslav Yefisko
+ Version     : 0
+ Copyright   : MIT License
+ Description : Write a program to print your name and address.
+ ============================================================================
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Name: Viacheslav Yefisko\n");
+    printf("Address: Nikolaev city, Ukraine\n");
+
+    printf("\n=====> DONE <=====\n");
+
+    return 0;
+}
